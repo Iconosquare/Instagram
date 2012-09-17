7d811c88fb211b92a7c81efcbfa5b81138ad681e
gaets86 <gaetan.houssin@gmail.com>
2012-09-17 09:55:24 +0200

    Instagram Library

--- src/Instagram/Instagram.php
+++ src/Instagram/Instagram.php
@@ -67,47 +67,47 @@ class Instagram {
 
     private function request($method, $endpoint, $params = null)
     {
-//        if (preg_match('#^https?://#', $endpoint)) {
-//            $url = $endpoint;
-//        } else {
-//            $url = $this->getApiUrl($endpoint);
-//        }
-//
-//        if ($this->accessToken) {
-//            $params['access_token'] = $this->accessToken;
-//        } else {
-//            $params['client_id'] = $this->clientId;
-//            $params['client_secret'] = $this->clientSecret;
-//        }
-//
-//        if ($method === 'GET') {
-//            $url .= is_null($params) ? '' : '?'.http_build_query($params, '', '&');
-//        }
-//
-//        if ($method === 'DELETE') {
-//            $paramToken['access_token'] = $this->accessToken;
-//            $url .= is_null($paramToken) ? '' : '?'.http_build_query($paramToken, '', '&');
-//        }
-//
-//        $ch  = curl_init($url);
-//        curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($ch, CURLOPT_TIMEOUT, $this->requestTimeout);
-//        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-//        if (isset($_SERVER ['SERVER_ADDR']) && !empty($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR'] != '127.0.0.1') {
-//            curl_setopt($ch, CURLOPT_INTERFACE, $_SERVER ['SERVER_ADDR']);
-//        }
-//        if ($method === 'POST' && $params !== null) {
-//            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-//        }
-//
-//        $body = curl_exec($ch);
-//		curl_close($ch);
-//        
-//		return $body;
+        if (preg_match('#^https?://#', $endpoint)) {
+            $url = $endpoint;
+        } else {
+            $url = $this->getApiUrl($endpoint);
+        }
+
+        if ($this->accessToken) {
+            $params['access_token'] = $this->accessToken;
+        } else {
+            $params['client_id'] = $this->clientId;
+            $params['client_secret'] = $this->clientSecret;
+        }
+
+        if ($method === 'GET') {
+            $url .= is_null($params) ? '' : '?'.http_build_query($params, '', '&');
+        }
+
+        if ($method === 'DELETE') {
+            $paramToken['access_token'] = $this->accessToken;
+            $url .= is_null($paramToken) ? '' : '?'.http_build_query($paramToken, '', '&');
+        }
+
+        $ch  = curl_init($url);
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->requestTimeout);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        if (isset($_SERVER ['SERVER_ADDR']) && !empty($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR'] != '127.0.0.1') {
+            curl_setopt($ch, CURLOPT_INTERFACE, $_SERVER ['SERVER_ADDR']);
+        }
+        if ($method === 'POST' && $params !== null) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
+
+        $body = curl_exec($ch);
+		curl_close($ch);
+        
+		return $body;
     }
     
 }
